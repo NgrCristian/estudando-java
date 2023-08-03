ee6c67929dd742b90e72855676e66e019768d6d8
NgrCristian <cristian.barreto.ne@gmail.com>
2023-08-03 13:26:45 -0300

    aula 0308

---
+++ src/aula0308/EstudoModificadores.java
@@ -0,0 +1,17 @@
+package aula0308;
+
+public class EstudoModificadores {
+//     Modificadores
+//    -Acesso
+//        ->public(pode acessar fora da classe e fora do pacote que ele foio definido)
+//        ->private(nao pode acessar fora da classe)
+//        ->protected(acessado somente pela propria classe e por seu filho)
+//        ->default(padrao) (acessado somente por classes no mesmo pacote)
+//    -nao Acesso
+//            ->static
+//            ->final
+//    Encapsulamento -> garantir a consistencia dos dados de uma classe
+public static void main(String[] args) {
+
+}
+}
